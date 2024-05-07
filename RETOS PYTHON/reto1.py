1d90fc3ca743c3d5ff59e0d4b1480642e3ca96da
dei8bit <dei8bit@gmail.com>
2024-05-06 22:07:06 -0300

    PYTHON primer reto terminado

--- RETOS PYTHON/reto1.py
+++ RETOS PYTHON/reto1.py
@@ -9,3 +9,47 @@
 # 📌 Crea una oración donde menciones el producto y su precio utilizando F-strings.
 # 📌 Valida el tipo de variable de tus variables de entero y texto.
 # 📌 Imprime en consola todas las operaciones que realizaste. 🧠
+
+
+entero = 20
+decimal = 22.3
+texto = "Aloha"
+booleano = True
+
+entero = 30
+texto = "hola♥"
+
+numeros = [0,9,6,7]
+
+frutas = ["🍍","🍅","🍓","🍉"]
+
+tupla = ("amarillo","celeste","violeta",[1,2,3,4,5])
+
+diccionario = {"manzana":15,"peras":13,"bananas":17}
+
+
+conjunto_numerico1 =  {1,2,2,3,4,5}
+conjunto_numerico2 = frozenset([1,2,3,4,5])
+
+variable = None
+
+producto = "televisor"
+precio = 245
+
+oracion = f"el prodcuto {producto} tiene un precio de: $ {precio}"
+
+
+print(entero)
+print(decimal)
+print(texto)
+print(booleano)
+print(numeros)
+print(frutas)
+print(tupla)
+print(diccionario)
+print(conjunto_numerico1)
+print(conjunto_numerico2)
+print(variable)
+print(oracion)
+print(type(entero))
+print(type(texto))
