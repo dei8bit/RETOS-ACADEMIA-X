0e26b79a7da8fca467f8fabab0687f600d3d8f27
dei8bit <dei8bit@gmail.com>
2024-05-07 20:38:59 -0300

    PYTHON reto 8 terminado

--- RETOS PYTHON/reto8.py
+++ RETOS PYTHON/reto8.py
@@ -1,4 +1,11 @@
 # Reto: Funciones incorporadas
 # 1. Crea un programa que de acuerdo a una lista de palabras solo imprima las que tienen más de 8 caracteres de longitud. 🧠
  
- 
+
+lista = ["anaconda","onomatopeya","hercules"]
+
+def palabras_largas(lista):
+  for item in lista:
+    if len(item)>8:print(f"{item} tiene mas de 8 caracteres")
+
+palabras_largas(lista)
