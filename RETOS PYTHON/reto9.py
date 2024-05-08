64c142a0ae5a61cd286b82ec1ec79ad701581564
dei8bit <dei8bit@gmail.com>
2024-05-08 17:13:19 -0300

    PYTHON reto 9 terminado

--- RETOS PYTHON/reto9.py
+++ RETOS PYTHON/reto9.py
@@ -1,4 +1,27 @@
 # Reto: Números
-# 1. Crea un programa que te permita convertir un número decimal a binario, octal o hexadecimal de acuerdo a la opción que elija un usuario mediante un menú en consola. 🧠
- 
- 
+# 1. Crea un programa que te permita convertir un número decimal a binario, octal o hexadecimal 
+# de acuerdo a la opción que elija un usuario mediante un menú en consola. 🧠
+
+
+option = False
+
+while option != True:
+    print("h = hexadecimal | o = octal | b = binario | q = salir\n")
+    try:
+        numero = int(input("ingrese un numero: "))
+    except ValueError:
+        print("error!!  , usted Ingreso un numero \n ")
+    option = input("ingrese una opcion: ").lower()
+
+    match option:
+        case "h":
+            print(f"Conversion hexadecimal: {hex(numero)}")
+        case "o":
+            print(f"Conversion octal: {oct(numero)}")
+        case "b":
+            print(f"Conversion binario: {bin(numero)}")
+        case "q":
+            print("saliendo...")
+            break
+        case _:
+            print("opcion incorrecta")
