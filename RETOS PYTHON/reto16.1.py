7944d6c9debf27ebd48a37b9f0d019b881e3c305
dei8bit <dei8bit@gmail.com>
2024-05-17 03:03:19 -0300

    PYTHON reto 16 terminado

---
+++ RETOS PYTHON/reto16.1.py
@@ -0,0 +1,15 @@
+from reto16 import Compra
+
+class Producto(): 
+  def __init__(self, nombre:str, precio:float, cantidad:int):
+      self.nombre = nombre
+      self.precio = precio
+      self.cantidad = cantidad
+
+
+
+miprod = Producto("cartuchera",3400,6)
+
+compra = Compra(miprod,2,"20-4-50")
+
+compra.mostrarFactura()
--- RETOS PYTHON/reto16.py
+++ RETOS PYTHON/reto16.py
@@ -1,4 +1,35 @@
 # Reto: Módulos
-# 1. Define una clase llamada Compra con los atributos de producto, cantidad y fecha actual de la compra, además crea un método para mostrar el total de la compra y un método para mostrar la factura. Crea la clase Producto en un archivo diferente con los atributos de nombre, precio y cantidad disponible. Importa la clase Compra e instancia un producto y utilízalo para instanciar una compra y generar una factura. 🧠
+# Define una clase llamada Compra con los atributos de producto, cantidad y fecha actual de la compra.
+# además crea un método para mostrar el total de la compra y un método para mostrar la factura. 
 
- 
+# Crea la clase Producto en un archivo diferente con los atributos de nombre, precio y cantidad disponible. 
+
+# Importa la clase Compra e instancia un producto y utilízalo para instanciar una compra y generar una factura.
+
+
+class Compra():
+  def __init__(self, producto:str, cantidad:int,  fechaActual:str):
+    self.producto = producto
+    self.cantidad = cantidad
+    self.fechaActual = fechaActual
+  
+  @property
+  def mostrarTotal(self):
+    return self.cantidad * self.producto.precio
+
+  def mostrarFactura(self):
+    total = self.mostrarTotal
+    print(f"""
+|------FACTURA------|
+      {self.fechaActual}
+Producto: {self.producto.nombre}
+Precio: $1230
+Stock: {self.cantidad} Unidades
+
+-o-o-o-o-o-o-o-o-o-
+        
+Unidades compradas: 
+Total: ${total}
+
+|___________________|\n
+""")
